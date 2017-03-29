dab891c9cd16440bf33092407ab65b44ba298a57
Alexander Raschid <alexraschid1337@gmail.com>
2017-03-29 21:48:52 +0000

    Got geoLocation workinggit add .git add ., Attempting at making a map pop up of user's location in the result field

--- js.js
+++ js.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
     var locationOn = false;
-    var locationCount = 0;
+    var locationCount = 1;
     var locationString;
     var lat;
     var long;
@@ -8,16 +8,13 @@ $(document).ready(function() {
                 long + "," + lat + "," +
                 "10/600x600.png?access_token=pk.eyJ1IjoiZG9kZ2VyNDg3IiwiYSI6ImNpeXcxY2xraDAwZHUyd21wam00NWc5NXIifQ.VNP3UdlAUjSJVz3_FrBkEQ";
     
-    var URL = "https://www.refugerestrooms.org:443/api/v1/restrooms/by_location.json" +
-            "?lat=" + lat +
-            "&lng=" + long;            
-                
                 
 
+                
     $('#button').on('click', function(event) {
         if(locationOn){
             //variable x marks the spotl geolocation method
-            x = navigator.geolocation;
+            var x = navigator.geolocation;
     
     
             //Calls the getCurrentPosition method 
@@ -25,41 +22,34 @@ $(document).ready(function() {
     
             //If the geolocation is able to be reached
             function success(position) {
+                //
                 lat = position.coords.latitude;
                 long = position.coords.longitude;
-        
                 
-    
+                console.log("Lat = " + lat);
+                console.log("Long = " + long);
+                
+                var geoLat = lat;
+                var geoLong = long;
+                
+                getGeoLocation(geoLat,geoLong);
+                
             }
-    
+            
+
     
             //A function for if the geolocation cannot be grabbed
             function failure() {
                 console.log("The current co-ordenates are unavailable");
             }
         }else{
+            //TODO: make string input converted into longitude and latitude
              locationString = $('#inputLocation').val();
              lat = locationString.substring(0, locationString.indexOf(' '));
              long = locationString.substring(locationString.indexOf(' ') + 1, locationString.length - 1);
         }
         
-
-        //calles the information from the API link
-        $.get(URL, function(data) {
-
-            var myresult = "";
-            for (var i = 0; i < 10; i++) {
-                myresult += "<li>------------------------------</li>";
-                myresult += "<li class='resultItem'>NAME: " + data[i].name + "</li>";
-                myresult += "<li class='resultItem'>STREET: " + data[i].street + "</li>";
-
-
-            }
-            $("#result").html(myresult);
-
-
-
-        })
+        
     });
 
 
@@ -70,16 +60,70 @@ $(document).ready(function() {
         if(locationCount % 2 == 0){
             locationOn = true;
             console.log("GeoLocation turned on");
+            $("#geoOn").css("background-color", "green")
             
         }else{
             locationOn = false;
             console.log("GeoLocation turned off");
+            $("#geoOn").css("background-color", "red")
+
 
         }
     });
+    
+    //gets locaiton
+    
+    function getGeoLocation(lat,long){
+         
+         var URL = "https://www.refugerestrooms.org:443/api/v1/restrooms/by_location.json" +
+            "?lat=" + lat +
+            "&lng=" + long;
+         
+         
+         $.get(URL, function(data) {
 
 
-})
+            var myresult = "";
+            $("#myresult").html(" ");
+            
+            for (var i = 0; i < 10; i++) {
+                //TODO add map around here
+                myresult += "<li>------------------------------</li>";
+                myresult += "<li class='resultItem'>NAME: " + data[i].name + "</li>";
+                myresult += "<li class='resultItem'>STREET: " + data[i].street + "</li>";
+                updateMap(lat,long, 15);
+
+
+            }
+            $("#result").html(myresult);
+
+
+
+        })
+        
+        
+    }
+    
+    
+    
+    
+    
+})    
+    
+    
+    
+
+
+
+
+function updateMap(lat, long, zoom){
+   
+    $("#map").attr("src", "https://api.mapbox.com/v4/mapbox.dark/0,0,3/600x600.png?access_token=pk.eyJ1IjoiZG9kZ2VyNDg3IiwiYSI6ImNpeXcxY2xraDAwZHUyd21wam00NWc5NXIifQ.VNP3UdlAUjSJVz3_FrBkEQ")
+    
+        var url = "https://api.mapbox.com/v4/mapbox.dark/" + long + "," + lat + "," + zoom + "/600x600.png?access_token=pk.eyJ1IjoiZG9kZ2VyNDg3IiwiYSI6ImNpeXcxY2xraDAwZHUyd21wam00NWc5NXIifQ.VNP3UdlAUjSJVz3_FrBkEQ"
+    
+        $("#map").attr("src", url);
+}
 
 
 
@@ -88,5 +132,5 @@ $(document).ready(function() {
 //https://www.refugerestrooms.org/api/docs/#!/restrooms/GET_version_restrooms_format
 //API key based on location
 //https://www.refugerestrooms.org:443/api/v1/restrooms/by_location.json?lat=40.7128%C2%B0&lng=-74.0059
-//HTML Geolocation:
-//
+// Mapbox API:
+// https://www.mapbox.com/api-documentation/?language=JavaScript#static
