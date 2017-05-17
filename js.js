c5be78b8256246a1c5b61121ce302ec5a612aa30
Alexander Raschid <alexraschid1337@gmail.com>
2017-05-17 21:39:38 +0000

    Cleaned js.js, attempted removal of redundant code/dev code comments/ console.logs

--- js.js
+++ js.js
@@ -14,77 +14,53 @@
 
 
  $(document).ready(function() {
-
-
-
-
      $('#button').on('click', function(event) {
          //If location is turned off, then manualInput is ran first
          //If location is on, then manual input wont be used.
+         
+         //Deals with manual input if geoLocation turned OFF
          if (!locationOn) {
-
-             //TODO: make string input converted into longitude and latitude
+             //Takes put from html bar
              locationString = $('#inputLocation').val();
+             //Geocodes manual input
              var geocodeURL = makeGeocodeURL(locationString);
-
-
+             //Adds markers/initalizs map on page
              $.get(geocodeURL, function(data) {
                  console.log(data);
-                 console.log("Latitude: " + data.results["0"].geometry.location.lat);
-                 console.log("Longitude: " + data.results["0"].geometry.location.lng);
-
                  let lat = data.results["0"].geometry.location.lat;
                  let lng = data.results["0"].geometry.location.lng;
-
                  initMap(lat, lng);
                  getLocation(lat, lng);
-
-
-
              });
-
-
-
-
+             
+         //Deals with geolocation
          } else {
              //variable x marks the spotl geolocation method
              var x = navigator.geolocation;
-
-
              //Calls the getCurrentPosition method 
              x.getCurrentPosition(success, failure);
-
              //If the geolocation is able to be reached
              function success(position) {
-                 //
+                 //capturing the lat and long co-ordinates of current position
                  let lat = position.coords.latitude;
                  let lng = position.coords.longitude;
-
-                 console.log("Lat = " + lat);
-                 console.log("Long hello= " + lng);
-                 console.log("After hello");
+                 
+                 //re assigns lat/lng to variables. Solves some type of error.
+                 //That wont parse
                  var geoLat = lat;
                  var geoLng = lng;
-                 console.log("After lat and lng set to new vars");
-                 console.log("b4 initMap");
+                 
+                 //Adds reveals map to page
                  initMap(lat, lng);
-                 console.log("after initMap");
                  getLocation(geoLat, geoLng);
-
              }
-
-
-
+             
              //A function for if the geolocation cannot be grabbed
              function failure() {
                  console.log("The current co-ordenates are unavailable");
              }
          }
-
-
      });
-
-
 
      //Toggles on and off the location
      $('#buttonGeo').on('click', function(event) {
@@ -103,77 +79,38 @@
          }
      });
 
-     //gets locaiton
-
+     //Function that retrieves the location, or more rather prints all the stores
+     //Onto the google map
      function getLocation(lat, lng) {
-
          var bathroomURL = "https://www.refugerestrooms.org:443/api/v1/restrooms/by_location.json" +
              "?&per_page=100" + //Number of object items in restroom APi
              "&lat=" + lat +
              "&lng=" + lng;
-
-         console.log("Making RefugeRestroom request with params:", lat, lng);
-
-
+             
+         //Parses api data through and prints the numofLocations
          $.get(bathroomURL, function(data) {
-             console.log(data);
-
-
-             var myresult = "";
-             $("#myresult").html(" ");
-
-
              var numOfLocations = 50;
-             for (var i = 0; i < numOfLocations; i++) {
-                 //TODO add map around here
-                 myresult += "<li>------------------------------</li>";
-                 myresult += "<li class='resultItem'>NAME: " + data[i].name + "</li>";
-                 myresult += "<li class='resultItem'>STREET: " + data[i].street + "</li>";
-                 myresult += "<li class='resultItem'>ACCESSIBLE: " + data[i].accessible + "</li>";
+             for (var i = 0; i < numOfLocations; i++) {   
                  addicon(mapGlobal, data, i);
-
-
              }
              $("#result").html(myresult);
-
-
-
          })
-
-
      }
-
-
-
-
  })
 
  //Removes and replaces all spaces with + in the string.
  //Then will pass it through to the API Key
- //v
-
- // content_copy
-
-
  function makeGeocodeURL(locationString) {
-
      //Splits the string into each character and seperates them with commas
-     //Using a for eac
-     let locationStringTemp = locationString.split(' ').join('+');
-
-
-     let geocodeKey = "https://maps.googleapis.com/maps/api/geocode/json?address=" + locationStringTemp + "&key=AIzaSyD1wqMJyEoUFEuvx1JffGaRgbeq1wRfngM";
-
-
+     var locationStringTemp = locationString.split(' ').join('+');
+     var geocodeKey = "https://maps.googleapis.com/maps/api/geocode/json?address=" + locationStringTemp + "&key=AIzaSyD1wqMJyEoUFEuvx1JffGaRgbeq1wRfngM";
      return geocodeKey;
-
  }
 
 
 
-
- function addicon(map, URL_DATA, DATA_INDEX) {
-
+  //Function to add a bathrooom icon
+   function addicon(map, URL_DATA, DATA_INDEX) {
      var marker = new google.maps.Marker({
          "position": {
              lat: URL_DATA[DATA_INDEX].latitude,
@@ -181,8 +118,11 @@
          },
          "map": map
      });
-     //var contentString = "<li class='resultItem'>NAME: " + URL_DATA[DATA_INDEX].name + "</li>"
 
+
+
+     //Adds the api api data foor different catigorys, such ass Street Adress and accessible
+     
      var infowindow = new google.maps.InfoWindow({
          content: "<li class='resultItem'><b>Store Name</b>: " + URL_DATA[DATA_INDEX].name + "</li>" +
              "<li class='resultItem'><b>Street Adress</b>: " + URL_DATA[DATA_INDEX].street + "</li>" +
@@ -204,36 +144,12 @@
          infowindow.open(map, marker);
 
      });
-
-
-     //  google.maps.event.addListener(marker, 'click', (function(marker, content, infowindow) {
-     //      return function() {
-     //          closeLastOpenedInfoWindo();
-     //          infowindow.setContent(content);
-     //          infowindow.open(map, marker);
-     //          lastOpenedInfoWindow = infowindow;
-     //      };
-     //  })(marker, makrerdata[i], infowindow));
-
-
-
-     //  function closeLastOpenedInfoWindo() {
-     //      if (lastOpenedInfoWindow) {
-     //          lastOpenedInfoWindow.close();
-     //      }
-     //  }
-
  }
 
 
 
-
- function initMap(lati, long) {
-     // var uluru = {
-     //     lat: lati,
-     //     lng: long
-     // };
-
+    //Function that sets the focus location of the map
+    function initMap(lati, long) {
      map = new google.maps.Map(document.getElementById('map'), {
          center: {
              lat: lati,
@@ -242,22 +158,21 @@
          zoom: 15,
          //center: uluru
      });
-     // var contentString = ;
+     
+     
+     
+     
+     //Code for making the center dot on the google map
      var infowindow = new google.maps.InfoWindow({
          content: "<h1>Current Location</h3>"
      });
-
-
-
-
      var pinColor = "41A317";
      var pinImage = new google.maps.MarkerImage("http://chart.apis.google.com/chart?chst=d_map_pin_letter&chld=%E2%80%A2|" + pinColor,
          new google.maps.Size(21, 34),
          new google.maps.Point(0, 0),
          new google.maps.Point(10, 34));
-
-
-     console.log("Marker attempts");
+         
+     //Function that adds the center marker/pin
      var marker = new google.maps.Marker({
          "position": {
              lat: lati,
@@ -271,25 +186,10 @@
      });
 
      mapGlobal = map;
-
-
-
-
-     // addicon(map, 40.750, -73.993, 'penn station');
-     // addicon(map, 40.754, -73.990, 'harlem');
  }
-
- // infoWindow = new google.maps.InfoWindow; //static infoWindow for all your markers
- // google.maps.event.addDomListener(window, 'load', function() {
- //   //create your markers here
- //   google.maps.event.addListener(marker, 'click', function() {
- //               infoWindow.open(map1, marker); //take care with case-sensitiveness
- //           });
- // });
 
  //API Documentation:
  //https://www.refugerestrooms.org/api/docs/#!/restrooms/GET_version_restrooms_format
+ 
  //API key based on location
  //https://www.refugerestrooms.org:443/api/v1/restrooms/by_location.json?lat=40.7128%C2%B0&lng=-74.0059
- // Mapbox API:
- // https://www.mapbox.com/api-documentation/?language=JavaScript#static
