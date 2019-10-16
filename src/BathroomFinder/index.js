4b101ee6dd8dccb57b5c124175489158631d601b
Alex Raschid <alexraschid1337@gmail.com>
2019-10-16 05:32:09 -0400

    Init Commit, Restarting project from scratch and this time with React.js

---
+++ src/BathroomFinder/index.js
@@ -0,0 +1,14 @@
+//https://www.refugerestrooms.org/api/docs/
+import React from 'react';
+
+export class BathroomFinder extends React.Component{
+    
+    
+    
+    
+    render() {
+        return (
+            <h1>Hello, World</h1>
+        );
+    }
+}
