7511ca5e19764e0acadabc1d72f304c67b25b670
Alex Raschid <alexraschid1337@gmail.com>
2019-10-16 23:11:03 -0400

    Implemented google-map-react, set union square as default center

--- src/BathroomFinder/map.js
+++ src/BathroomFinder/map.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import GoogleMapReact from 'google-map-react';
 
 export class BathroomMap extends React.Component{
 
@@ -6,50 +7,27 @@ export class BathroomMap extends React.Component{
         super(props);
         this.state = {
             API_KEY: "AIzaSyCZ6cSChM_EVpFY_iYNeo_plSDBogPYoPY",
-            API: "https://maps.googleapis.com/maps/api/js?key="+ this.API_KEY +"&callback=initMap"
+            API: "https://maps.googleapis.com/maps/api/js?key="+ this.API_KEY +"&callback=initMap",
+            center: {
+                lat: 40.7367,
+                lng: -73.9899
+              },
+              zoom: 15
         }
     }
 
-    componentDidMount(){
-        this.loadMap();
-    }
-
-
-    loadMap = () =>{
-        this.loadScript("https://maps.googleapis.com/maps/api/js?key="+ this.state.API_KEY +"&callback=initMap");
-        window.initMap = this.initMap;
-    }
-
-    initMap = () => {
-        const map = new window.google.maps.Map(document.getElementById('map_canvas', {
-            center: {lat: -34.397, lng: 150.644},
-            zoom: 8
-        }));
-
-        //return map;
-    }
-
-    loadScript(url){
-        const index = window.document.getElementsByTagName("script")[0];
-        const script = window.document.createElement("script");
-        script.src = url;
-        script.type = 'text/javascript';
-        script.async = true;
-        script.defer = true;
-        index.parentNode.insertBefore(script, index);
-        
-    }
-    /*
-        <script async defer
-        src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&callback=initMap">
-        </script>
-    */
-
-
-
     render() {
         return (
-           <main> <div id="map_canvas"></div></main>
+            <div style={{ height: '100vh', width: '100%' }}>
+                <GoogleMapReact
+                    bootstrapURLKeys={{key: this.state.API_KEY}}
+                    defaultCenter={this.state.center}
+                    defaultZoom={this.state.zoom}
+                >
+                    
+                </GoogleMapReact>
+
+            </div>
         );
     }
 }
