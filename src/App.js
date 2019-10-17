26d75dc5abf7619d945e43bb25d1f3cbc568f595
Alex Raschid <alexraschid1337@gmail.com>
2019-10-17 09:19:03 -0400

    Added NavBar to Page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import { BathroomMap } from './BathroomFinder/map.js';
+import { NavBar } from './BathroomFinder/navbar.js';
 import { Grid } from '@material-ui/core';
 
 function App() {
   return (
     <div className="App">
+      <NavBar></NavBar>
       <BathroomMap></BathroomMap>
     </div>
   );
