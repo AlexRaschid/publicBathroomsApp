62ccf0dd28c45de9955118641cf51e09b4e64f60
AlexRaschid <alexraschid1337@gmail.com>
2019-10-21 11:23:26 -0400

    Got marker to appear on screen; no functionality

--- src/BathroomFinder/map.js
+++ src/BathroomFinder/map.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react';
+//https://www.refugerestrooms.org/api/v1/restrooms/by_location?page=1&per_page=20&offset=0&lat=40.7367&lng=-73.9899
+
+
 
 export class BathroomMap extends React.Component{
 
     constructor(props){
         super(props);
         this.state = {
-            API_KEY: "AIzaSyCZ6cSChM_EVpFY_iYNeo_plSDBogPYoPY",
-            API: "https://maps.googleapis.com/maps/api/js?key="+ this.API_KEY +"&callback=initMap",
+            refugeAPI: "https://www.refugerestrooms.org/api/v1/restrooms/by_location?page=1&per_page=20&offset=0&lat=40.7367&lng=-73.9899",
+            gmapsAPI: "https://maps.googleapis.com/maps/api/js?key="+ this.API_KEY +"&callback=initMap",
             center: {
                 lat: 40.7367,
                 lng: -73.9899
@@ -16,13 +19,30 @@ export class BathroomMap extends React.Component{
             showingInfoWindow: false,
             activeMarker: {},
             selectedPlace: {},
+            infoWindowOpen: false,
         }
+
+        this.handleToggleOpen = this.handleToggleOpen.bind(this);
+    }
+
+    handleToggleOpen = () => {
+        console.log("Marker Clicked");
+        this.setState({
+           infoWindowOpen: !this.state.infoWindowOpen,
+        });
     }
 
     render() {
         return (
             
                 <Map google={window.google} zoom={14} initialCenter={this.state.center}>
+                    <Marker onClick={this.handleToggleOpen}
+                        name={'Current location'} />
+                    <InfoWindow open={this.state.infoWindowOpen} onClose={this.onInfoWindowClose}>
+                        <div>
+                            <h1>Marker Info Placeholder</h1>
+                        </div>
+                    </InfoWindow>
                 </Map>
             
         );
